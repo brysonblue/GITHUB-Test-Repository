412c94618683510a8062af3bbb1e743b308916fe
brysonblue <128836043+brysonblue@users.noreply.github.com>
2024-07-12 09:18:34 -0400

    Adding more to the Python file. Adding inputs. Fixed the color question.

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 print ("Hello, what is your name?")
 input()
-print(input('Nice to meet you!'))
-print("What is your favorite color?")
-input()
-print(input("That's great! my favorite color is red!"))
+print(input('Nice to meet you! What is your favorite color? '))
+print("That's great! my favorite color is red!")
