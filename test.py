f9a5cb407a67a743c94104d158f8760f5c34f64a
brysonblue <128836043+brysonblue@users.noreply.github.com>
2024-07-12 09:08:48 -0400

    Adding more to the Python file. Adding inputs.

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print ("Hello World")
+print ("Hello, what is your name?")
+input()
+print(input('Nice to meet you!'))
+print("What is your favorite color?")
+input()
+print(input("That's great! my favorite color is red!"))
